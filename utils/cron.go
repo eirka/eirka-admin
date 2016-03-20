b78cac83db5b42f429b15070f3f10c7f83a473e1
techjanitor <puntme@gmail.com>
2016-03-19 22:22:43 -0500

    add cron util and analytics pruner

---
+++ utils/cron.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"github.com/robfig/cron"
+
+	"github.com/eirka/eirka-libs/db"
+)
+
+func init() {
+
+	c := cron.New()
+
+	// prune old analytics
+	c.AddFunc("@midnight", PruneAnalytics())
+
+	c.Start()
+}
+
+// Will prune the analytics table
+func PruneAnalytics() (err error) {
+
+	// Get Database handle
+	dbase, err := db.GetDb()
+	if err != nil {
+		return
+	}
+
+	_, err = dbase.Exec("DELETE FROM analytics WHERE request_time < (now() - interval 1 month)")
+	if err != nil {
+		return
+	}
+
+	return
+
+}
